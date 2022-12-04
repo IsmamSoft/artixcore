dca4b584ff3c4b15001683bbd8afe47817f37b33
Ismam Tabriz <116435327+IsmamSoft@users.noreply.github.com>
2022-12-05 03:05:46 +0600

    modified:   resources/js/components/pages/welcome/index.vue

--- resources/js/components/pages/welcome/index.vue
+++ resources/js/components/pages/welcome/index.vue
@@ -3,6 +3,5 @@ import Header from '../layouts/header.vue'
 </script>
 <template>
    <Header />
-    <router-link to="/login">Login</router-link>
-    <router-link to="/register">Register</router-link>
+
 </template>
