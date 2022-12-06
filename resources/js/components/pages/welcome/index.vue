0427bbe686fa135e438ec951952bfbf998cbf687
Ismam Tabriz <116435327+IsmamSoft@users.noreply.github.com>
2022-12-07 04:18:59 +0600

    d

--- resources/js/components/pages/welcome/index.vue
+++ resources/js/components/pages/welcome/index.vue
@@ -3,5 +3,40 @@ import Header from '../layouts/header.vue'
 </script>
 <template>
    <Header />
+   <div class="py-2"></div>
+   <div class="container py-2">
+    <div class="row">
+        <div class="col-md-6">
+            <div class="card">
+                <img src="https://media.istockphoto.com/id/1317323736/photo/a-view-up-into-the-trees-direction-sky.jpg?s=170667a&w=0&k=20&c=Sh2L3BedE75yIli2x1Y-A_Gz9MDluBIrH-Ff3BLRvyk=" alt="" srcset="">
+            <div class="card-body">
+            </div>
+        </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card">
+                <img src="https://media.istockphoto.com/id/1317323736/photo/a-view-up-into-the-trees-direction-sky.jpg?s=170667a&w=0&k=20&c=Sh2L3BedE75yIli2x1Y-A_Gz9MDluBIrH-Ff3BLRvyk=" alt="" srcset="">
+            <div class="card-body">
+                dsfksdkjfsdklfsdklf
+            </div>
+            </div>
+            <!-- <br> -->
+            <div class="card">
+                <img src="https://media.istockphoto.com/id/1317323736/photo/a-view-up-into-the-trees-direction-sky.jpg?s=170667a&w=0&k=20&c=Sh2L3BedE75yIli2x1Y-A_Gz9MDluBIrH-Ff3BLRvyk=" alt="" srcset="">
+            <div class="card-body">
+                dsfksdkjfsdklfsdklf
+            </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card">
+                <img src="https://media.istockphoto.com/id/1317323736/photo/a-view-up-into-the-trees-direction-sky.jpg?s=170667a&w=0&k=20&c=Sh2L3BedE75yIli2x1Y-A_Gz9MDluBIrH-Ff3BLRvyk=" alt="" srcset="">
+            <div class="card-body">
+                dsfksdkjfsdklfsdklf
+            </div>
+        </div>
+        </div>
 
+    </div>
+   </div>
 </template>
